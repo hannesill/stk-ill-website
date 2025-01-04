f0e662b953ce1a065d961bd1e4318a8723c9accf
hill <hannes.ill@tum.de>
2025-01-04 17:24:37 +0100

    Fix deployment bugs

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/steuerkanzlei-ill' : '',
+			base: process.env.NODE_ENV === 'production' ? '/stk-ill-website' : '',
 		}
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	base: 'steuerkanzlei-ill/'
+	base: '/stk-ill-website/'
 });
