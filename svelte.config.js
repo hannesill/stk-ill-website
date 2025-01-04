9043b71d0f3441e96460d9c107051d32c09ea456
hill <hannes.ill@tum.de>
2025-01-04 17:16:29 +0100

    Fix build errors

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/stk-ill-website/' : '',
+			base: process.env.NODE_ENV === 'production' ? '/steuerkanzlei-ill' : '',
 		}
 	}
 };
